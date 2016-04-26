60f16e9062fa2e911efbee248ca5a1da7b3b55b2
tomek <romen3@gmail.com>
2016-04-26 22:41:44 +0200

    First commit! Walking character, etc

---
+++ Assets/CharacterFriction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFriction : MonoBehaviour {
+	public Vector2 force;
+	private bool isWPressed = false;
+	private bool isSPressed = false;
+	private bool isAPressed = false;
+	private bool isDPressed = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckForKeyDownsAndUps ();
+		Friction ();
+	}
+
+	void CheckForKeyDownsAndUps() {
+		isWPressed &= !Input.GetKeyUp (KeyCode.W);
+		isWPressed |= Input.GetKeyDown (KeyCode.W);
+		isSPressed &= !Input.GetKeyUp (KeyCode.S);
+		isSPressed |= Input.GetKeyDown (KeyCode.S);
+		isAPressed &= !Input.GetKeyUp (KeyCode.A);
+		isAPressed |= Input.GetKeyDown (KeyCode.A);
+		isDPressed &= !Input.GetKeyUp (KeyCode.D);
+		isDPressed |= Input.GetKeyDown (KeyCode.D);
+	}
+
+	void Friction() {
+		Vector2 velocity = GetComponent<Rigidbody2D> ().velocity;
+		if (!isWPressed && !isSPressed) {
+			if (velocity.y < 0)
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, force.x));
+			else if (velocity.y > 0)
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, -force.x));
+		}
+		if (!isAPressed && !isDPressed) {
+			if (velocity.x < 0)
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (force.y, 0));
+			else if (velocity.x > 0)
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-force.y, 0));
+			
+		}
+		print (velocity);
+	}
+}
---
+++ Assets/WSADControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WSADControl : MonoBehaviour {
+	private bool isWPressed = false;
+	private bool isSPressed = false;
+	private bool isAPressed = false;
+	private bool isDPressed = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isWPressed |= Input.GetKeyDown (KeyCode.W);
+		isWPressed &= !Input.GetKeyUp (KeyCode.W);
+		isSPressed |= Input.GetKeyDown (KeyCode.S);
+		isSPressed &= !Input.GetKeyUp (KeyCode.S);
+		isAPressed |= Input.GetKeyDown (KeyCode.A);
+		isAPressed &= !Input.GetKeyUp (KeyCode.A);
+		isDPressed |= Input.GetKeyDown (KeyCode.D);
+		isDPressed &= !Input.GetKeyUp (KeyCode.D);
+		Move ();
+	}
+
+	void Move() {
+		if (isWPressed) {
+			if (Math.Abs (GetComponent<Rigidbody2D> ().velocity.y) < 2.0)
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, 2), ForceMode2D.Force);				
+		} else if (isSPressed) {
+			if (Math.Abs (GetComponent<Rigidbody2D> ().velocity.y) < 2.0 )
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, -2), ForceMode2D.Force);
+		} 
+		if (isDPressed) {
+			if (Math.Abs (GetComponent<Rigidbody2D> ().velocity.x) < 2.0 )
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (2, 0), ForceMode2D.Force);
+		} else if (isAPressed) {
+			if (Math.Abs (GetComponent<Rigidbody2D> ().velocity.x) < 2.0 )
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-2, 0), ForceMode2D.Force);
+		}
+	}
+
+
+}
